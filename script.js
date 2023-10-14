1a8b75a94e6634810332a18ac4a3725883044ce0
AIDA9898 <138362134+Eliza2105@users.noreply.github.com>
2023-10-14 15:35:23 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+//  if- эгерде
+// else - болбосо
+
+// let year = prompt('Кыргызстандын эгемендик жылы кайсы жылы тузулгон')
+
+// if(year ==1991){
+//     alert('туура,азаматсын')
+
+// }else{
+//     alert('Туура эмес болуп калды дагы аракет кылып кор')
+// }
+
+// let password = prompt('Пароль жаз')
+// if(password == 'asd123'){
+//     alert('true')
+
+// }else if (password == 'asd124'){
+//     alert('Кичине жакындадын дагы аракет кыл')
+// }else{
+//     alert('false')
+// }
+
+// const courseStatus = 'ready'
+
+// if(courseStatus =='ready'){
+//     alert('курс башталды')
+// }else if(courseStatus  == 'pending'){
+//     alert('курс даярдалып жатат')
+// }else{
+//     alert ('курс болбойт')
+// }
+
+
+// while - пока что
+
+// let  i= 1
+
+// while(i<=4){
+//     alert(i)
+//     i=i+1
+//     i++
+// }
+
+// for(башталышы,щарт,шаг){
+
+// }for(let x = 1; x<10; x++){
+//     alert('x')
+// }
